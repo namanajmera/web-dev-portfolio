46ae199779cf9357b643a4f02fe89a51845e4fd1
Naman Ajmera <cool.naman.ajmera@gmail.com>
2024-02-11 14:54:02 +0530

    Added: Home Page Completed.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 import big_land_computer from "./big_land_computer.svg";
+import download from "./download.svg";
 
-export { big_land_computer };
+export { big_land_computer, download };
--- src/components/Home Component/HomeComponent.tsx
+++ src/components/Home Component/HomeComponent.tsx
@@ -1,34 +1,46 @@
 import React, { useState } from "react";
 import styles from "./HomeComponent.module.scss";
-import { big_land_computer } from "../../assets/images";
+import { big_land_computer, download } from "../../assets/images";
 
 type Props = {};
 
 const HomeComponent = (props: Props) => {
-    const [profileVisit] = useState([
-        {
-          id: 1,
-          title: "githubLink",
-          link: "https://github.com/namanAjmera",
-          userName: "@namanAjmera",
-        },
-        {
-          id: 2,
-          title: "linkedIn",
-          link: "https://www.linkedin.com/in/namanajmera",
-          userName: "@namanajmera",
-        },
-        {
-          id: 3,
-          title: "leetCode",
-          link: "https://leetcode.com/namanAjmera",
-          userName: "@namanAjmera",
-        },
-      ]);
+  const [profileVisit] = useState([
+    {
+      id: 1,
+      title: "githubLink",
+      link: "https://github.com/namanAjmera",
+      userName: "@namanAjmera",
+      tag: "GitHub/@namanAjmera",
+    },
+    {
+      id: 2,
+      title: "linkedInLink",
+      link: "https://linkedin.com/in/namanajmera",
+      userName: "@namanajmera",
+      tag: "LinkedIn/@namanAjmera",
+    },
+    {
+      id: 3,
+      title: "leetCodeLink",
+      link: "https://leetcode.com/namanAjmera",
+      userName: "@namanAjmera",
+      tag: "Leetcode/@namanAjmera",
+    },
+  ]);
+
+  const downloadResume = () => {
+    const aTag = document.createElement("a");
+    aTag.href =
+      "https://drive.google.com/file/d/1NjkF_sB0abVAGa6SBQvNABATMuq2CKCM/view?usp=sharing";
+    aTag.target = "_blank";
+    aTag.download = "Naman's Resume.pdf";
+    aTag.click();
+  };
   return (
-    <div className={styles['home-container']}>
+    <div className={styles["home-container"]}>
       <div className={styles["left-side"]}>
-      <div className={styles["selfIntroduction"]}>
+        <div className={styles["selfIntroduction"]}>
           <span className={styles["initial"]}>{"Hi all. I am"}</span>
           <span className={styles["selfName"]}>{"Naman Ajmera"}</span>
           <span className={styles["about-me-title"]}>
@@ -49,15 +61,25 @@ const HomeComponent = (props: Props) => {
                   className={styles["value"]}
                   rel="noreferrer"
                 >
-                  {ele.userName}
+                  {ele.tag}
                 </a>
                 {'"'}
               </p>
             ))}
         </div>
+        <div className={styles["downloads"]}>
+          <button className={styles["dwn-btn"]} onClick={downloadResume}>
+            <img src={download} alt="" className={styles["dwn-img"]} />
+            <span className={styles["dwn-text"]}>_Resume</span>
+          </button>
+        </div>
       </div>
       <div className={styles["right-side"]}>
-        <img className={styles["big_land_computer"]} src={big_land_computer} alt="" />
+        <img
+          className={styles["big_land_computer"]}
+          src={big_land_computer}
+          alt=""
+        />
       </div>
     </div>
   );

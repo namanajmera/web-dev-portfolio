ea53460d0a7f18a1aa7e30545d91f6ea613483e8
Naman Ajmera <cool.naman.ajmera@gmail.com>
2024-02-25 21:33:37 +0530

    Added: Contact Me Functional Done.

--- src/components/ContactMe Component/ContactMeComponent.tsx
+++ src/components/ContactMe Component/ContactMeComponent.tsx
@@ -4,6 +4,29 @@ import IdBadgeComponent from "../IdBadgeComponent/IdBadgeComponent";
 type Props = {};
 
 const ContactMeComponent = (props: Props) => {
+  const handleLink = (social: string) => {
+    if (social === "insta") {
+      window.open("https://www.instagram.com/hackernaman/", "_blank");
+    } else if (social === "linkedIn") {
+      window.open("https://www.linkedin.com/in/namanajmera/", "_blank");
+    } else {
+      window.open("https://github.com/namanajmera", "_blank");
+    }
+  };
+
+  const handleMail = (email: string) => {
+    const subject = "Contact Me";
+    const body = `Can you share me the ideas of projects.`;
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoLink;
+  };
+
+  const handleCall = (phoneNumber: number) => {
+    const telLink = `tel:${phoneNumber}`;
+    window.location.href = telLink;
+  };
   return (
     <div className={styles["contact-container"]}>
       <div className={styles["top-container"]}>
@@ -18,16 +41,24 @@ const ContactMeComponent = (props: Props) => {
             </p>
             <p className={styles["key-value"]}>
               <span className={styles["key"]}>{"email: "}</span>
-              <span className={styles["value"]}>
-                {"'cool.naman.ajmera@gmail.com;'"}
+              <span
+                className={styles["value"]}
+                onClick={() => handleMail("cool.naman.ajmera@gmail.com")}
+              >
+                {"'cool.naman.ajmera@gmail.com;,'"}
               </span>
             </p>
             <p className={styles["key-value"]}>
               <span className={styles["key"]}>{"phone: "}</span>
-              <span className={styles["value"]}>{"+91-8696696426;"}</span>
+              <span
+                className={styles["value"]}
+                onClick={() => handleCall(8696696426)}
+              >
+                {"+91-8696696426;,"}
+              </span>
             </p>
             <p>
-              <span className={styles["curly"]}>{"}"}</span>
+              <span className={styles["curly"]}>{"},"}</span>
             </p>
           </div>
           <div className={styles["socialMedia"]}>
@@ -37,15 +68,30 @@ const ContactMeComponent = (props: Props) => {
             </p>
             <p className={styles["key-value"]}>
               <span className={styles["key"]}>{"instagram: "}</span>
-              <span className={styles["value"]}>{" '@hackerNaman';"}</span>
+              <span
+                className={styles["value"]}
+                onClick={() => handleLink("insta")}
+              >
+                {" '@hackerNaman';,"}
+              </span>
             </p>
             <p className={styles["key-value"]}>
               <span className={styles["key"]}>{"github: "}</span>
-              <span className={styles["value"]}>{" '@namanAjmera';"}</span>
+              <span
+                className={styles["value"]}
+                onClick={() => handleLink("github")}
+              >
+                {" '@namanAjmera';,"}
+              </span>
             </p>
             <p className={styles["key-value"]}>
               <span className={styles["key"]}>{"linkedIn: "}</span>
-              <span className={styles["value"]}>{" '@namanAjmera';"}</span>
+              <span
+                className={styles["value"]}
+                onClick={() => handleLink("linkedIn")}
+              >
+                {" '@namanAjmera';,"}
+              </span>
             </p>
             <p>
               <span className={styles["curly"]}>{"}"}</span>

ab1b9dca92819e1e6bd63f1783d446885c3814d3
Naman Ajmera <cool.naman.ajmera@gmail.com>
2024-02-25 20:22:58 +0530

    Added: Complete About Me.

--- src/components/IdBadgeComponent/IdBadgeComponent.tsx
+++ src/components/IdBadgeComponent/IdBadgeComponent.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./IdBadgeComponent.module.scss";
-import { Me, instagram, facebook, github, twitter } from "../../assets/images";
+import { Me, instagram, github, twitter } from "../../assets/images";
 
 type Props = {};
 
 const IdBadgeComponent = (props: Props) => {
+  const [profileVisit] = useState([
+    {
+      id: 1,
+      link: "https://www.instagram.com/hackernaman/",
+      userName: "@hackernaman",
+      imgUrl: instagram,
+    },
+    {
+      id: 2,
+      link: "https://github.com/namanajmera",
+      userName: "@namanAjmera",
+      imgUrl: github,
+    },
+    {
+      id: 3,
+      link: "https://twitter.com/hacker__Naman",
+      userName: "@namanAjmera",
+      imgUrl: twitter,
+    },
+  ]);
+
+  const handleProfiles = (
+    event: React.MouseEvent<HTMLElement>,
+    profile: any
+  ) => {
+    event.preventDefault();
+    window.open(profile.link, "_blank");
+  };
   return (
     <div className={styles["badge-container"]}>
       <div className={styles["face-logo"]}>
@@ -13,12 +41,20 @@ const IdBadgeComponent = (props: Props) => {
         </span>
       </div>
       <div className={styles["self-tag"]}>
-        <span className={styles["self"]}> @NamanAjmera </span>
+        <span className={styles["self"]}> @namanAjmera </span>
         <div className={styles["profile-container"]}>
-          <img src={instagram} alt="" className={styles["profiles"]} />
-          <img src={facebook} alt="" className={styles["profiles"]} />
+          {profileVisit &&
+            profileVisit.map((ele, index) => (
+              <img
+                src={ele.imgUrl}
+                alt=""
+                className={styles["profiles"]}
+                onClick={(event) => handleProfiles(event, ele)}
+              />
+            ))}
+          {/* <img src={facebook} alt="" className={styles["profiles"]} />
           <img src={github} alt="" className={styles["profiles"]} />
-          <img src={twitter} alt="" className={styles["profiles"]} />
+          <img src={twitter} alt="" className={styles["profiles"]} /> */}
         </div>
       </div>
     </div>
